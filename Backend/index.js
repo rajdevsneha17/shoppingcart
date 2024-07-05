e04645bede0ca7d5ca0017111951b163db1937d8
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-05 10:42:28 +0530

    indexmodify

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,18 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 credentials:true
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://shoppingcart-fss.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        // Handle preflight requests
+        res.header('Access-Control-Allow-Origin', 'https://shoppingcart-fss.vercel.app');
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json())
 
 const routes=require("./routes/sendmail")
